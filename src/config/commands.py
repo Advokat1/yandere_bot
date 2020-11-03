f0c1ee57e320047998050a64f2f109a35d1ce2d1
Egor Fadeev <contact@muindor.com>
2020-11-03 20:47:46 +0300

    bug fix

--- src/config/commands.py
+++ src/config/commands.py
@@ -2,9 +2,9 @@ from aiogram.types import BotCommand
 
 list_of_commands = [
     {
-        command: "test_states",
-        description: "Проверяет работу 'состояний'"
+        'command': "test_states",
+        'description': "Проверяет работу 'состояний'"
     }
 ]
 
-commands = [BotCommand(i.command, d.description) for i in list_of_commands]
+commands = [BotCommand(i['command'], i['description']) for i in list_of_commands]
